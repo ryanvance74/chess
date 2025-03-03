bc80e74c71765c172b1f11e06a704a2058c4a2ab
ryanvance74 <58126311+ryanvance74@users.noreply.github.com>
2025-03-03 00:44:54 -0700

    saving wip changes

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -72,7 +72,7 @@ class RookMovesCalculator implements PieceMovesCalculator {
         Collection<int[]> rookDirectionArray = new ArrayList<>();
         for (int x = -1; x < 2; x++) {
             for (int y = -1; y < 2; y++) {
-                // only accept diagonals
+                // only accept straights
                 if (!(x == 0 && y == 0) && (x == 0 || y == 0) ) {
                     rookDirectionArray.add(new int[]{x,y});
                 }
